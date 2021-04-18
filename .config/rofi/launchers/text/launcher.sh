07c22996fab246785b3e94decdd830d38b5182b9
ビクセル <vcyzscape@gmail.com>
2021-04-18 21:13:21 +0700

    bspwm: refactor some config
    Signed-off-by: ビクセル <vcyzscape@gmail.com>

--- .config/rofi/launchers/text/launcher.sh
+++ .config/rofi/launchers/text/launcher.sh
@@ -10,7 +10,7 @@
 #
 # style_1     style_2     style_3     style_4     style_5     style_6     style_7
 
-theme="style_1"
+theme="style_5"
 
 dir="$HOME/.config/rofi/launchers/text"
 styles=($(ls -p --hide="colors.rasi" $dir/styles))
